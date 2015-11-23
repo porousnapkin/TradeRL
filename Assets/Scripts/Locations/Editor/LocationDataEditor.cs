2350cc6d0a5dbe743d89075c4789c2bae47538a0
Will Swannack <willswannack@Wills-MacBook-Pro.local>
2015-11-22 19:15:54 -0800

    Missed these files.

---
+++ Assets/Scripts/Locations/Editor/LocationDataEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(LocationData))]
+public class LocationDataEditor : Editor {	
+	LocationData locationData;
+
+	public override void OnInspectorGUI() {
+		locationData = target as LocationData;
+
+		locationData.locationName = EditorGUILayout.TextField("Name", locationData.locationName);
+		locationData.description = EditorGUILayout.TextField("Description", locationData.description);
+		locationData.art = EditorGUILayout.ObjectField("Art", locationData.art, typeof(Sprite), false) as Sprite;
+		locationData.type = (LocationType)EditorGUILayout.EnumPopup("Type", locationData.type);
+
+		switch(locationData.type) {
+		case LocationType.ConstantStory:
+			ShowConstantStory(); break;
+		case LocationType.OneOffStory:
+			ShowOneOffStory(); break;
+		case LocationType.ActiveStoryWithCooldown:
+			ShowActiveStoryWithCooldown(); break;
+		}
+	}
+
+	void ShowConstantStory() {
+		locationData.firstStory = EditorGUILayout.ObjectField("Story", locationData.firstStory, typeof(StoryData), false) as StoryData;
+	}
+
+	void ShowOneOffStory() {
+		locationData.firstStory = EditorGUILayout.ObjectField("Story", locationData.firstStory, typeof(StoryData), false) as StoryData;
+		locationData.secondStory = EditorGUILayout.ObjectField("Deactive Story", locationData.secondStory, typeof(StoryData), false) as StoryData;
+	}
+
+	void ShowActiveStoryWithCooldown() {
+		locationData.firstStory = EditorGUILayout.ObjectField("Story", locationData.firstStory, typeof(StoryData), false) as StoryData;
+		locationData.secondStory = EditorGUILayout.ObjectField("Deactive Story", locationData.secondStory, typeof(StoryData), false) as StoryData;
+		locationData.cooldownTurns = EditorGUILayout.IntField("Cooldown Turns", locationData.cooldownTurns);
+	}
+}
---
+++ Assets/Scripts/Locations/LocationData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum LocationType {
+	OneOffStory,
+	ActiveStoryWithCooldown,
+	ConstantStory,
+}
+
+public class LocationData : ScriptableObject {
+	public string locationName;
+	public string description;
+	public Sprite art;
+	public LocationType type;
+	public int cooldownTurns;
+	public StoryData firstStory;
+	public StoryData secondStory;
+}
