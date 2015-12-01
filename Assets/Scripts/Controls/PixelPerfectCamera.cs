9bbc54c9dea10ca67d28d15e6a67d780ace5d181
Swannack <wswannack@sea-14770-mac.internal.popcap.com>
2015-12-01 14:13:33 -0800

    Camera starts in correct position.

--- Assets/Scripts/Controls/PixelPerfectCamera.cs
+++ Assets/Scripts/Controls/PixelPerfectCamera.cs
@@ -10,15 +10,28 @@ public class PixelPerfectCamera : MonoBehaviour {
 	void Start () {
 		unitsPerPixel = 100;
 		// Camera.main.orthographicSize = (Screen.height / 2f) / unitsPerPixel;
+		Invoke ("ImmediateMoveToGoalPosition", 0.01f);
+	}
+
+	void ImmediateMoveToGoalPosition() {
+		transform.position = PixelPerfectizePosition (GetGoalPosition ());
 	}
 
 	void Update() {
-		Vector2 worldPos = playerController.playerCharacter.WorldPosition;
-		var goalPosition = Grid.GetCharacterWorldPositionFromGridPositon((int)worldPos.x, (int)worldPos.y);
+		var goalPosition = GetGoalPosition ();
 		var outPosition = Vector3.Lerp(transform.position, new Vector3(goalPosition.x, goalPosition.y, transform.position.z), closenessPercent);
 
-		transform.position = new Vector3(RoundToCloseness(outPosition.x, 0.01f), RoundToCloseness(outPosition.y, 0.01f), outPosition.z);
-		// transform.position = outPosition;
+		transform.position = PixelPerfectizePosition (outPosition);
+	}
+
+	Vector3 GetGoalPosition() {
+		Vector2 worldPos = playerController.playerCharacter.WorldPosition;
+		var goalPosition = Grid.GetCharacterWorldPositionFromGridPositon((int)worldPos.x, (int)worldPos.y);
+		return goalPosition;
+	}
+
+	Vector3 PixelPerfectizePosition(Vector3 pos) {
+		return new Vector3(RoundToCloseness(pos.x, 0.01f), RoundToCloseness(pos.y, 0.01f), pos.z);
 	}
 
 	float RoundToCloseness(float input, float closeness) {
