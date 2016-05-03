c137add2f561eea75f931f6f5fc8b2e9898446aa
Will Swannack <porousnapkin@gmail.com>
2016-05-02 21:54:03 -0700

    Removing supplies from the game because they're not fun.

---
+++ Assets/Scripts/Story/ActionEvents/Data/GainCoinsEventData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GainCoinsEventData : StoryActionEventData
+{
+    public int numCoins;
+
+    public override StoryActionEvent Create()
+    {
+        var e = DesertContext.StrangeNew<GainCoinsEvent>();
+        e.coins = numCoins;
+        return e;
+    }
+}
---
+++ Assets/Scripts/Story/ActionEvents/GainCoinsEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GainCoinsEvent : StoryActionEvent
+{
+    [Inject]
+    public Inventory inventory { private get; set; }
+    public int coins = 0;
+
+    public void Activate()
+    {
+        inventory.Gold += coins;
+    }
+}
