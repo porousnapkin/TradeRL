c752444d21733544ea6e753699534af51d4e0179
Will Swannack <porousnapkin@gmail.com>
2016-08-05 21:09:39 -0700

    Mised this.

---
+++ Assets/Scripts/UI/CombatUI.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class CombatUI : MonoBehaviour {
+	void Start () {
+        gameObject.SetActive(false);
+        GlobalEvents.CombatStarted += () => gameObject.SetActive(true);
+        GlobalEvents.CombatEnded += () => gameObject.SetActive(false);
+	}
+}
