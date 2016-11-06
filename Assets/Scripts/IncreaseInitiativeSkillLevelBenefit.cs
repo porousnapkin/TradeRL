4be9c4f97cb10685f7dd7e2d3afb79e963356397
Will Swannack <porousnapkin@gmail.com>
2016-11-05 21:45:22 -0700

    Added necessary stuff for speed skill.

---
+++ Assets/Scripts/IncreaseInitiativeSkillLevelBenefit.cs
@@ -0,0 +1,10 @@
+public class IncreaseInitiativeSkillLevelBenefit : SkillLevelBenefit
+{
+    public int amount;
+
+    public override void Apply(PlayerCharacter playerCharacter)
+    {
+        playerCharacter.GetCharacter().speed += amount;
+    }
+}
+
