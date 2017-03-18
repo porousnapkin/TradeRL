ec84e50c58248ca625a587b449a46645007a145d
Will Swannack <porousnapkin@gmail.com>
2017-03-17 21:31:40 -0700

    missed adding these files.

---
+++ Assets/Scripts/CopyOtherRectTransformValues.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CopyOtherRectTransformValues : MonoBehaviour {
+    public RectTransform toCopy;
+    public RectTransform myTransform;
+
+	void Update () {
+        myTransform.anchoredPosition = toCopy.anchoredPosition;
+        myTransform.anchorMax = toCopy.anchorMax;
+        myTransform.anchorMin = toCopy.anchorMin;
+        myTransform.offsetMax = toCopy.offsetMax;
+        myTransform.offsetMin = toCopy.offsetMin;
+        myTransform.pivot = toCopy.pivot;
+	}
+}
