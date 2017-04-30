be939a55d44979afbcb03040c80268d33af06d76
Will Swannack <porousnapkin@gmail.com>
2017-04-30 07:41:04 -0700

    The health and defense bars now lerp and white flash.

--- Assets/Scripts/Bar.cs
+++ Assets/Scripts/Bar.cs
@@ -1,14 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Bar : MonoBehaviour {
+    public Image imageThatFlashes;
     public RectTransform barTransform;
-    float percent;
+    public RectTransform burnTransform;
+    public float lerpTime = 0.5f;
+    public bool whiteFlashOnLowered = true;
+    public float whiteFlashTime = 0.1f;
+    Color defaultColor;
+    float oldPercent = 1.0f;
+
+    void Start()
+    {
+        defaultColor = imageThatFlashes.color;
+    }
 
     public void SetPercent(float percent) {
-        this.percent = percent;
+        LeanTween.cancel(gameObject);
 
-        barTransform.localScale = new Vector3(percent, 1, 1);
+        if (burnTransform == null)
+            barTransform.localScale = new Vector3(percent, 1, 1);
+        else if (percent < oldPercent)
+            SetupLowered(percent);
+        else
+            SetupRaised(percent);
+
+        oldPercent = percent;
 	}
+
+    private void SetupRaised(float percent)
+    {
+        barTransform.localScale = new Vector3(oldPercent, 1, 1);
+        burnTransform.localScale = new Vector3(percent, 1, 1);
+        var capturedOld = oldPercent;
+        LeanTween.value(gameObject, (p) => barTransform.localScale = new Vector3(p, 1, 1), capturedOld, percent, lerpTime);
+    }
+
+    private void SetupLowered(float percent)
+    {
+        burnTransform.localScale = new Vector3(oldPercent, 1, 1);
+        barTransform.localScale = new Vector3(percent, 1, 1);
+        var capturedOld = oldPercent;
+        LeanTween.value(gameObject, (p) => burnTransform.localScale = new Vector3(p, 1, 1), capturedOld, percent, lerpTime);
+
+        if (whiteFlashOnLowered)
+            WhiteFlash();
+    }
+
+    void WhiteFlash()
+    {
+        imageThatFlashes.color = Color.white;
+        LeanTween.value(gameObject, (c) => imageThatFlashes.color = c, Color.white, defaultColor, whiteFlashTime).setOnComplete(() => imageThatFlashes.color = defaultColor);
+    }
 }
